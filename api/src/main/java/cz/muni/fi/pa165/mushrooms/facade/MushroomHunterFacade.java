c7c8fa468361bd4f18439c6569445447eb25000d
Ludmila Fialová <xfialov3@fi.muni.cz>
2017-11-18 15:52:41 +0100

    facade javadoc repaired

--- api/src/main/java/cz/muni/fi/pa165/mushrooms/facade/MushroomHunterFacade.java
+++ api/src/main/java/cz/muni/fi/pa165/mushrooms/facade/MushroomHunterFacade.java
@@ -8,23 +8,4 @@ import java.util.List;
  * @author bencikpeter, bohdancvejn, bkompis, Lindar84, Buvko
  */
 public interface MushroomHunterFacade {
-
-    MushroomHunterDTO findHunterById(Long userId); // TODO: do we want ID's in DTO? o.O
-
-    MushroomHunterDTO findHunterByNickname(String userNickname);
-
-    void registerHunter(MushroomHunterDTO hunter, String unencryptedPassword);
-
-    void deleteHunter(MushroomHunterDTO hunter);
-
-    void updateHunter(MushroomHunterDTO hunter);
-
-    void updatePassword(MushroomHunterDTO hunter, String oldUnencryptedPassword,
-                        String newUnencryptedPassword);
-
-    List<MushroomHunterDTO> findAllHunters();
-
-    boolean authenticate(MushroomHunterDTO user);
-
-    boolean isAdmin(MushroomHunterDTO user);
 }
