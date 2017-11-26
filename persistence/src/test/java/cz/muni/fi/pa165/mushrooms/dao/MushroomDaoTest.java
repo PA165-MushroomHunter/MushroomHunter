198a091aca5920cbaa751e9ca6780aa07fceb08b
Ludmila Fialová <xfialov3@fi.muni.cz>
2017-11-26 06:39:38 +0100

    MushroomFacadeImplTest

--- persistence/src/test/java/cz/muni/fi/pa165/mushrooms/dao/MushroomDaoTest.java
+++ persistence/src/test/java/cz/muni/fi/pa165/mushrooms/dao/MushroomDaoTest.java
@@ -104,7 +104,7 @@ public class MushroomDaoTest extends AbstractTransactionalJUnit4SpringContextTes
         assertThatThrownBy(() -> mushroomDao.create(mushroom)).isInstanceOf(ConstraintViolationException.class);
     }
 
-    //this string validation is not implemented yet - tests prepared for the future
+    // TODO this string validation is not implemented yet - tests prepared for the future
 //    @Test
 //    public void create_nullBeginMonth() throws Exception {
 //        Mushroom mushroom = createMushroom("WierdMushroom", MushroomType.UNEDIBLE, null, "July");
